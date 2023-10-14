533e952ded68b73efac6dde09e97c2488e79b2f4
Dima <dorogovd@yandex.ru>
2023-10-14 17:41:19 +0300

    update

--- YandexViews/GameFieldView.swift
+++ YandexViews/GameFieldView.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class gameFieldView: UIView { // Отрисовка фигуры внутри поля вручную
     var shapeColor: UIColor = .red // цвет фигуры - значение по умолчанию
+    var shapePosition: CGPoint = .zero // координата верхнего левого угла прямоугольника для размещения фигуры
+    var shapeSize: CGSize = CGSize(width: 40, height: 40) // размер этого прямоугольника
     override func draw(_ rect: CGRect) { // переопределяем метод дроу рект для ручной отрисовки внутри представления (view)
         super.draw(rect) // вызов реализации медода из класса родителя
         shapeColor.setFill() // устанавливаем свой цвет
+        let path = getTrianglePath(in: CGRect(origin: shapePosition, size: shapeSize))
+        path.fill()
     }
     
     private func getTrianglePath(in rect: CGRect) -> UIBezierPath {
